ab3ef373063ad733147cb1f0ffd2129a9acca3c8
DamianLarrea <damiantlarrea@gmail.com>
2023-11-02 22:53:29 +1100

    Add new registration extension methods

---
+++ App/App.Console/Extensions/OptionsRegistration.cs
@@ -0,0 +1,15 @@
+﻿using App.Data;
+using Microsoft.Extensions.Hosting;
+
+namespace Microsoft.Extensions.DependencyInjection
+{
+    internal static class OptionsRegistration
+    {
+        public static HostApplicationBuilder ConfigureOptions(this HostApplicationBuilder builder)
+        {
+            builder.Services.Configure<UserApiOptions>(builder.Configuration.GetSection(nameof(UserApiOptions)));
+
+            return builder;
+        }
+    }
+}
---
+++ App/App.Console/Extensions/ServiceRegistration.cs
@@ -0,0 +1,26 @@
+﻿using App.Core;
+using App.Data;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace App.Console.Extensions
+{
+    internal static class ServiceRegistration
+    {
+        public static HostApplicationBuilder RegisterServices(this HostApplicationBuilder builder)
+        {
+            builder.Services.AddScoped<Program>();
+            builder.Services.AddScoped<UserService>();
+            builder.Services.AddScoped<IUserRepository, UserRepository>();
+
+            builder.Services.AddHttpClient();
+
+            return builder;
+        }
+    }
+}

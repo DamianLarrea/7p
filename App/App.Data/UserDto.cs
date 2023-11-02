b482069704c72851f0fd015dcfc14cb8b770fa1b
DamianLarrea <damiantlarrea@gmail.com>
2023-11-02 21:23:18 +1100

    Add user repository and API options

---
+++ App/App.Data/UserDto.cs
@@ -0,0 +1,16 @@
+﻿namespace App.Data
+{
+    internal class UserDto
+    {
+        public int Id { get; set; }
+
+        public int Age { get; set; }
+
+        public string FirstName { get; set; } = string.Empty;
+        
+        public string LastName { get; set; } = string.Empty;
+        
+        public string Gender { get; set; } = string.Empty;
+    }
+}
+
---
+++ App/App.Data/UserRepository.cs
@@ -0,0 +1,29 @@
+﻿using App.Core;
+using Microsoft.Extensions.Options;
+using System.Net.Http.Json;
+
+namespace App.Data
+{
+    public class UserRepository : IUserRepository
+    {
+        private readonly IHttpClientFactory _httpClientFactory;
+        private readonly UserApiOptions _apiOptions;
+
+        public UserRepository(IHttpClientFactory httpClientFactory, IOptions<UserApiOptions> options)
+        {
+            _httpClientFactory = httpClientFactory;
+            _apiOptions = options.Value;
+        }
+
+        public async Task<IEnumerable<User>> GetUsers()
+        {
+            var client = _httpClientFactory.CreateClient();
+
+            var response = await client.GetFromJsonAsync<IEnumerable<UserDto>>(_apiOptions.Url);
+
+            return response?.Select(MapToUser).ToList() ?? new List<User>();
+        }
+
+        private static User MapToUser(UserDto dto) => new User(dto.Id, dto.Age, dto.FirstName, dto.LastName, dto.Gender);
+    }
+}
